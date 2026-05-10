571c40664b29477dfc14683269be6e113c7d66be
Subash-G-S <subashgopi2006@gmail.com>
2026-05-10 17:19:00 +0530

    added progressive scroll bar

--- script.js
+++ script.js
@@ -21,3 +21,12 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   }
 });
+// SCROLL PROGRESS BAR
+window.addEventListener("scroll", () => {
+  const scrollTop = window.scrollY;
+  const docHeight = document.documentElement.scrollHeight - window.innerHeight;
+
+  const progress = (scrollTop / docHeight) * 100;
+
+  document.querySelector(".scroll-progress").style.width = progress + "%";
+});
